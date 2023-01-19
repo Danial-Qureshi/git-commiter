06895b21a8ef9af8e6f0f52e87b13b8f007c69d0
Daniyal Amjed <danial@vidivet.com>
2023-01-19 15:23:20 +0500

    Commit for 2023-01-19 - 1

--- gitScript.sh
+++ gitScript.sh
@@ -8,12 +8,3 @@ textVar=$1
 
 # Git commit
 git commit -am "Commit for $textVar"
-
-# Define the date you want to set (format: yyyy-MM-dd)
-$NewDate = "2024-01-19"
-
-# Set the system date and time
-Set-Date -Date $NewDate
-
-# Display the updated date and time
-Get-Date
